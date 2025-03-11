ef24698a19054f2d54e8e9c5d062e314597a6fd5
valentindush <codesvalentin@gmail.com>
2025-03-11 10:26:25 +0200

    initial commit

---
+++ client/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "QT URLS - URL Shortener",
+  description: "Shorten your URLs quickly and easily with QT URLS. The best URL shortener service.",
+  keywords: ["URL shortener", "link shortener", "shorten URLs", "QT URLS"],
+  authors: [{ name: "QT Global"}],
+  viewport: "width=device-width, initial-scale=1.0",
+  robots: "index, follow",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
